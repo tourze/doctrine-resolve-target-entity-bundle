b6bd4734bbf12790aba12b3a246e8c8b78788a1a
github-ci <you@example.com>
2025-11-04 18:50:46 +0800

    update

--- tests/DoctrineResolveTargetEntityBundleTest.php
+++ tests/DoctrineResolveTargetEntityBundleTest.php
@@ -5,15 +5,19 @@ declare(strict_types=1);
 namespace Tourze\DoctrineResolveTargetEntityBundle\Tests;
 
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\DoctrineResolveTargetEntityBundle\DoctrineResolveTargetEntityBundle;
-use Tourze\PHPUnitSymfonyKernelTest\AbstractBundleTestCase;
 
 /**
  * @internal
+ * 这个测试用例，不允许依赖 phpunit-symfony-kernel-test
  */
 #[CoversClass(DoctrineResolveTargetEntityBundle::class)]
-#[RunTestsInSeparateProcesses]
-final class DoctrineResolveTargetEntityBundleTest extends AbstractBundleTestCase
+final class DoctrineResolveTargetEntityBundleTest extends TestCase
 {
+    public function testBundleClass()
+    {
+        self::assertTrue(is_subclass_of(DoctrineResolveTargetEntityBundle::class, Bundle::class));
+    }
 }
